04a4e7bd60f86d204666aff44cc0e0ef4b5bf0b1
MattAGUT <mapache.tn@gmail.com>
2023-05-04 20:00:12 -0400

    Even more

---
+++ pages/Lab10Basemaps.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import leafmap.folium as leafmap
+
+st.set_page_config(layout = "wide")
+
+col1, col2 = st.colums([4,1])
+
+with col2:
+    dropdown= st.selectbox("Basemap", ["Hybrid" , "Roadmap", "Terrain", "Satellite"])
+
+    url = st.text_input("Enter URL")
+
+m = leafmap.Map()
+m.add_basemap(dropdown)
+
+if url:
+    m.add_tile_layer(url, name = "Tile Layer", attribution= " ")
+
+with col1:m.to_streamlit()
