7bcee0edbcf4d813c567fd0162bacd79e70387ed
MattAGUT <mapache.tn@gmail.com>
2023-05-04 20:17:42 -0400

    again again

--- pages/Lab10Basemaps.py
+++ pages/Lab10Basemaps.py
@@ -6,7 +6,7 @@ st.set_page_config(layout = "wide")
 col1, col2 = st.columns([4,1])
 
 with col2:
-    dropdown= st.selectbox("Basemap", ["Hybrid" , "Roadmap", "Terrain", "Satellite"])
+    dropdown = st.selectbox("Basemap", ["Hybrid" , "Roadmap", "Terrain", "Satellite"])
 
     url = st.text_input("Enter URL")
 
