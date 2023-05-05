5c6e08b2ca3e6762de9d48f80fb41ddafbc2a867
MattAGUT <mapache.tn@gmail.com>
2023-05-04 20:48:38 -0400

    making changes

--- pages/Lab10Basemaps.py
+++ pages/Lab10Basemaps.py
@@ -5,10 +5,15 @@ st.set_page_config(layout = "wide")
 
 col1, col2 = st.columns([4,1])
 
-with col2:
-    dropdown = st.selectbox("Basemap", ["HYBRID" , "ROADMAP", "TERRAIN", "SATELLITE"])
+options = list(leafmap.basemaps.keys())
 
-    url = st.text_input("Enter URL")
+
+with col2:
+    dropdown = st.selectbox("Basemap", options)
+
+    default_url = leafmap.basemaps[dropdown].tiles
+
+    url = st.text_input("Enter URL", default_url)
 
 m = leafmap.Map()
 m.add_basemap(dropdown)
