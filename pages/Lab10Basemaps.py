50e61b30bff711d20df6d289695169d586469eda
MattAGUT <mapache.tn@gmail.com>
2023-05-04 20:05:22 -0400

    mas mas

--- pages/Lab10Basemaps.py
+++ pages/Lab10Basemaps.py
@@ -3,7 +3,7 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout = "wide")
 
-col1, col2 = st.colums([4,1])
+col1, col2 = st.columns([4,1])
 
 with col2:
     dropdown= st.selectbox("Basemap", ["Hybrid" , "Roadmap", "Terrain", "Satellite"])
